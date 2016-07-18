1cc6be16cb5d5c9e1d74f5145ee73ddf6e48259e
chrisweb <chris963@gmail.com>
2016-07-19 01:32:27 +0200

    subpage scrapping

--- universal-nodejs-harvester/universal-nodejs-harvester/server.ts
+++ universal-nodejs-harvester/universal-nodejs-harvester/server.ts
@@ -16,6 +16,67 @@ let _ = require('lodash');
 // https://github.com/chrisweb/chrisweb-utilities.js
 let utilities = require('chrisweb-utilities');
 
+let scrapDetails = function (urlToScrap, callback) {
+
+    request(urlToScrap, function (error, response, html) {
+
+        if (!error && response.statusCode == 200) {
+
+            utilities.log('finished harvesting details, now extracting metadata ...', 'fontColor:yellow');
+
+            let $ = cheerio.load(html);
+
+            let result = {};
+
+            let $body = $('body');
+            let $mainContent = $body.find('.maincontent');
+            let $rowsChildren = $mainContent.children('.row');
+            let $thirdRow = $rowsChildren.eq(2);
+            let $detailsTable = $thirdRow.find('table');
+            //let $detailsTableBody = $detailsTable.find('tbody');
+            let $tableRows = $detailsTable.children('tr');
+
+            $tableRows.each(function (index, element) {
+
+                let $element = $(element);
+                let $columns = $element.find('td');
+                let $secondColumn = $columns.eq(1);
+
+                let secondColumnContent = $secondColumn.text().replace(/\\t/g, '').replace(/\s+/g, '').trim();
+
+                switch (index) {
+                    case 1:
+                        result['address'] = secondColumnContent || '';
+                        break;
+                    case 2:
+                        result['telephone'] = secondColumnContent || '';
+                        break;
+                    case 3:
+                        result['mobile'] = secondColumnContent || '';
+                        break;
+                    case 4:
+                        result['email'] = secondColumnContent || '';
+                        break;
+                    case 5:
+                        result['website'] = secondColumnContent || '';
+                        break;
+                }
+
+            });
+
+            callback(null, result);
+
+        } else {
+
+            callback(error, '');
+
+        }
+
+
+    });
+
+};
+
 let pageCounter = 1;
 
 let scrap = function (urlToScrap, callback) {
@@ -41,54 +102,78 @@ let scrap = function (urlToScrap, callback) {
             let $tableBody = $table.find('tbody');
             let $allRows = $tableBody.children('tr');
 
+            let allRowsLength = $allRows.length;
+
             $allRows.each(function (index, element) {
 
-                let $element = $(element);
+                // wait 2 seconds then do next call
+                setTimeout(() => {
 
-                let $titleColumn = $element.find('.cspacer.ca3');
-                let $countryColumn = $element.find('.cspacer.ca4');
-                let $hallsColumn = $element.find('.cspacer.ca5');
-                let $boothsColumn = $element.find('.cspacer.ca6');
+                    let $element = $(element);
+
+                    let $titleColumn = $element.find('.cspacer.ca3');
+                    let $countryColumn = $element.find('.cspacer.ca4');
+                    let $hallsColumn = $element.find('.cspacer.ca5');
+                    let $boothsColumn = $element.find('.cspacer.ca6');
                 
-                let $titleColumnLink = $titleColumn.find('a');
-                let detailsUrl = domainToScrap + $titleColumnLink.prop('href');
-                let name = $titleColumnLink.text().trim();
+                    let $titleColumnLink = $titleColumn.find('a');
+                    let detailsUrl = domainToScrap + $titleColumnLink.prop('href');
+                    let name = $titleColumnLink.text().trim();
 
-                let country = $countryColumn.text().trim();
+                    let country = $countryColumn.text().trim();
 
-                let halls = $hallsColumn.text().trim().replace(/<br>/g, ' / ');
+                    let halls = $hallsColumn.html().trim().replace(/<br>/g, ' / ').replace(/&#xA0;/g, ' ');
 
-                let booths = $boothsColumn.text().trim().replace(/<br>/g, ' / ');
+                    let booths = $boothsColumn.html().trim().replace(/<br>/g, ' / ').replace(/&#xA0;/g, ' ');
 
-                // metadata
-                var metadata = {
-                    //detailsUrl: detailsUrl,
-                    name: name,
-                    country: country,
-                    halls: halls,
-                    booths: booths
-                };
+                    // metadata
+                    let coreMetadata = {
+                        name: name,
+                        country: country,
+                        halls: halls,
+                        booths: booths
+                    };
 
-                parsedResults.push(metadata);
+                    // now scrap the details page
+                    // TODO: there should only be one scrapper
+                    scrapDetails(detailsUrl, (error, detailsMetadata) => {
+
+                        if (!error) {
+
+                            let metadata = _.assign(coreMetadata, detailsMetadata);
+
+                            parsedResults.push(metadata);
+
+                        }
+
+                        if (index === allRowsLength - 1) {
+                        
+                            // find links to other pages (through pagination)
+                            let $pager = $body.find('.pager');
+                            let $lastLinkElement = $pager.find('a').last();
+                            let lastLinkUrl = $lastLinkElement.prop('href');
+
+                            let nextPageUrl = null;
+                            let lastLinkContent = parseInt($lastLinkElement.text().trim());
+
+                            // if the content of the last link last link is NaN it means that
+                            // it contains an arrow image, so we have a next page, otherweise
+                            // if it is numeric it means we have reached the end
+                            if (_.isNaN(lastLinkContent)) {
+                                nextPageUrl = domainToScrap + lastLinkUrl;
+                            }
+
+                            
+                            callback(null, parsedResults, nextPageUrl);
+
+
+                        }
+
+                    });
+
+                }, 2000);
 
             });
-            
-            // find links to other pages (through pagination)
-            let $pager = $body.find('.pager');
-            let $lastLinkElement = $pager.find('a').last();
-            let lastLinkUrl = $lastLinkElement.prop('href');
-
-            let nextPageUrl = null;
-            let lastLinkContent = parseInt($lastLinkElement.text().trim());
-
-            // if the content of the last link last link is NaN it means that
-            // it contains an arrow image, so we have a next page, otherweise
-            // if it is numeric it means we have reached the end
-            if (_.isNaN(lastLinkContent)) {
-                nextPageUrl = domainToScrap + lastLinkUrl;
-            }
-
-            callback(null, parsedResults, nextPageUrl);
 
         } else {
 
@@ -147,14 +232,14 @@ let execute = function (startUrlToScrap) {
 
         if (!error) {
 
-            _.union(results, parsedResults);
+            results = _.union(results, parsedResults);
 
             if (!_.isNull(nextPageUrl)) {
 
-                // wait 5 seconds then do next call
+                // wait 2 seconds then do next call
                 setTimeout(() => {
-                    execute(nextPageUrl)
-                }, 5000);
+                    execute(nextPageUrl);
+                }, 2000);
 
             } else {
 
