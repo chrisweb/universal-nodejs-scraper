1c872b7545f8f4f9be6f302474ca326a39ebe30c
chrisweb <chris963@gmail.com>
2020-08-12 15:12:38 +0200

    remove unused dependencies

--- scripts/dist.js
+++ scripts/dist.js
@@ -1,5 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 //import pkg from '../package.json';
+
 export default {
   input: 'src/index.ts',
   external: [ 'cheerio', 'json2csv', 'chrisweb-utilities', 'fs', 'https', 'path' ],
@@ -7,7 +8,7 @@ export default {
     {
       //file: pkg.module,
       dir: 'dist',
-      //format: 'esm',
+      //format: 'esm', // use when less problems external modules that are not esm but cjs only
       format: 'cjs',
       sourcemap: true,
     },
